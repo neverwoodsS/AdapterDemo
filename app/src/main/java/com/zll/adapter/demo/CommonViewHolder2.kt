671879f4c72dd4674226188b7aed26846f2de8b9
zhanglulin <413700858@qq.com>
2019-06-10 10:29:48 +0800

    CommonViewHolder2

---
+++ app/src/main/java/com/zll/adapter/demo/CommonViewHolder2.kt
@@ -0,0 +1,12 @@
+package com.zll.adapter.demo
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+
+/**
+ * Created on 2019-06-10
+ * Author: zhangll
+ * Email: 413700858@qq.com
+ */
+class CommonViewHolder2(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
